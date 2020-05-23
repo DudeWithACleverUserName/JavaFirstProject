7ab9e463b564d36af40b5f3bca3d4f8a5f709c02
DudeWithACleverUserName <yogagg234@gmail.com>
2020-05-24 00:34:30 +0530

    Code Refactoring

--- src/main/java/com/sapient/springIOC/Employee/App.java
+++ src/main/java/com/sapient/springIOC/Employee/App.java
@@ -8,22 +8,33 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
 
-	public static boolean findEmployee(EmployeeBlueprint temp, int id) {
+	public static Scanner scanner = new Scanner(System.in);
+
+	public static void findEmployee(ApplicationContext ctx) {
+
+		System.out.print("Enter employee id:  ");
+		int id = scanner.nextInt();
 
 		boolean found = false;
+		for (int i = 1; i < 6; i++) {
 
-		if (temp.getId() == id) {
+			EmployeeBlueprint temp = (EmployeeBlueprint) ctx.getBean("emp" + i);
 
-			System.out.println("Details of the Employee:");
-			System.out.println("Id: " + temp.getId());
-			System.out.println("Name: " + temp.getName());
-			System.out.println("City: " + temp.getCity());
-			found = true;
+			if (temp.getId() == id) {
+
+				System.out.println("Details of the Employee:");
+				System.out.println("Id: " + temp.getId());
+				System.out.println("Name: " + temp.getName());
+				System.out.println("City: " + temp.getCity());
+				found = true;
+			}
 		}
-		return found;
+		if (!found) {
+			System.out.println("No employee found with id: " + id);
+		}
+
 	}
 
-	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 
 		System.out.println("/*-------Part-1------*/");
@@ -31,42 +42,14 @@ public class App {
 
 		ApplicationContext context = new ClassPathXmlApplicationContext("bean.xml");
 
-		Scanner scanner = new Scanner(System.in);
-
-		System.out.print("Enter employee id:  ");
-		int id = scanner.nextInt();
-		boolean found = false;
-
-		for (int i = 1; i < 6; i++) {
-
-			EmployeeBlueprint temp = (EmployeeBlueprint) context.getBean("emp" + i);
-			found = found | findEmployee(temp, id);
-
-		}
-
-		if (!found) {
-			System.out.println("No employee found with id: " + id);
-		}
+		findEmployee(context);
 
 		System.out.println("\n/*-------Part-2------*/");
 		System.out.println("Using Java Based Container");
 
 		ApplicationContext ctx = new AnnotationConfigApplicationContext(EmployeeConfig.class);
 
-		System.out.print("Enter employee id:  ");
-		id = scanner.nextInt();
-		found = false;
-
-		for (int i = 1; i < 6; i++) {
-
-			EmployeeBlueprint temp = (EmployeeBlueprint) ctx.getBean("emp" + i);
-			found = found | findEmployee(temp, id);
-
-		}
-
-		if (!found) {
-			System.out.println("No employee found with id: " + id);
-		}
+		findEmployee(ctx);
 
 		scanner.close();
 
